c93d1c448bd89abb126cfa3a897e489e91e65d18
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-09-05 09:58:56 +0100

    Initial commit

---
+++ src/main/java/com/daitamer/Record/Shop/API/RecordShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.daitamer.Record.Shop.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopApiApplication.class, args);
+	}
+
+}
