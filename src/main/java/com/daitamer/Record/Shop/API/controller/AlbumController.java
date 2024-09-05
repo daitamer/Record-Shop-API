42b4c7229466482cef56eeb320a5708a239bcf52
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-09-05 16:28:45 +0100

    feat: add AlbumController class, no implementation

---
+++ src/main/java/com/daitamer/Record/Shop/API/controller/AlbumController.java
@@ -0,0 +1,52 @@
+package com.daitamer.Record.Shop.API.controller;
+
+import com.daitamer.Record.Shop.API.model.Album;
+import com.daitamer.Record.Shop.API.service.AlbumService;
+import com.daitamer.Record.Shop.API.service.AlbumServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumController {
+
+    @Autowired
+    private AlbumService albumService;
+
+//    get all albums
+    @GetMapping
+    public List<Album> getAllAlbums() {
+
+    }
+
+
+//    get album by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Album> getAlbumById(@PathVariable Long id){
+
+    }
+
+//    create album
+    @PostMapping
+    public ResponseEntity<Album> createAlbum(@RequestBody Album album){
+
+    }
+
+//    update album
+    @PutMapping
+    @PostMapping("/{id}")
+    public ResponseEntity<Album> updateAlbum(@PathVariable Long id, @RequestBody Album album){
+
+    }
+
+//    delete album by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAlbumById(){
+
+    }
+
+
+}
---
+++ src/test/java/com/daitamer/Record/Shop/API/controller/AlbumControllerTest.java
@@ -0,0 +1,24 @@
+package com.daitamer.Record.Shop.API.controller;
+
+import com.daitamer.Record.Shop.API.model.Album;
+import com.daitamer.Record.Shop.API.model.Genre;
+import com.daitamer.Record.Shop.API.repository.AlbumRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+
+public class AlbumControllerTest {
+
+
+
+}
