ec0625106f50997cb905687eef9a5bf260e7eeda
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-09-06 10:35:27 +0100

    test: controller test testGetAllAlbums as part of TDD

--- src/main/java/com/daitamer/Record/Shop/API/controller/AlbumController.java
+++ src/main/java/com/daitamer/Record/Shop/API/controller/AlbumController.java
@@ -15,38 +15,38 @@ public class AlbumController {
 
     @Autowired
     private AlbumService albumService;
-
-//    get all albums
-    @GetMapping
-    public List<Album> getAllAlbums() {
-
-    }
-
-
-//    get album by id
-    @GetMapping("/{id}")
-    public ResponseEntity<Album> getAlbumById(@PathVariable Long id){
-
-    }
-
-//    create album
-    @PostMapping
-    public ResponseEntity<Album> createAlbum(@RequestBody Album album){
-
-    }
-
-//    update album
-    @PutMapping
-    @PostMapping("/{id}")
-    public ResponseEntity<Album> updateAlbum(@PathVariable Long id, @RequestBody Album album){
-
-    }
-
-//    delete album by id
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAlbumById(){
-
-    }
+//
+////    get all albums
+//    @GetMapping
+//    public List<Album> getAllAlbums() {
+//
+//    }
+//
+//
+////    get album by id
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Album> getAlbumById(@PathVariable Long id){
+//
+//    }
+//
+////    create album
+//    @PostMapping
+//    public ResponseEntity<Album> createAlbum(@RequestBody Album album){
+//
+//    }
+//
+////    update album
+//    @PutMapping
+//    @PostMapping("/{id}")
+//    public ResponseEntity<Album> updateAlbum(@PathVariable Long id, @RequestBody Album album){
+//
+//    }
+//
+////    delete album by id
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deleteAlbumById(){
+//
+//    }
 
 
 }
--- src/test/java/com/daitamer/Record/Shop/API/controller/AlbumControllerTest.java
+++ src/test/java/com/daitamer/Record/Shop/API/controller/AlbumControllerTest.java
@@ -2,23 +2,61 @@ package com.daitamer.Record.Shop.API.controller;
 
 import com.daitamer.Record.Shop.API.model.Album;
 import com.daitamer.Record.Shop.API.model.Genre;
-import com.daitamer.Record.Shop.API.repository.AlbumRepository;
+import com.daitamer.Record.Shop.API.service.AlbumService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
-
+@WebMvcTest(AlbumController.class)
 public class AlbumControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AlbumService albumService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Album album;
+
+    @BeforeEach
+    void setUp(){
+        album = new Album(1L, "Album1", "Artist1", Genre.ROCK, 1969, 10.00);
+    }
+
+    @Test
+    void testGetAllAlbums() throws Exception {
+        //arrange
+
+        given(albumService.getAllAlbums()).willReturn(List.of(album));
+
+        //act
+        //assert
+        mvc.perform(get("/albums")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(album)));
+
+        verify(albumService, times(1)).getAllAlbums();
+    }
 
 
 }
